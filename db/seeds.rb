3ae7cce1055ac9562d2c967d602d22bd1267613b
Kristian Ruiz Kyvik <kristianruizkyvik@gmail.com>
2014-08-23 20:56:03 +0200

    fixed logic of game and added soundfs

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@ quiz = Quiz.create(title: "rap-test", user: user)
 question=Question.create(
   title: "what is the name of the song?",
   song: "65rRB2mspD309xE6YimZTl",
+  image_url: "https://i.scdn.co/image/c19574975c0ac24f69ab0529c495b44cb87a4353",
   quiz_id: quiz.id)
 
 Choice.create(title: "Money of my mind", question_id:question.id, correct: true)
